154b2c2720d0a723974ce73255850f7b334b464e
Istvan Viczian <viczian.istvan@gmail.com>
2025-12-16 13:48:06 +0100

    Loader kezdeti projekt létrehozása

---
+++ courses-loader/src/main/java/training/coursesloader/CoursesLoaderApplication.java
@@ -0,0 +1,13 @@
+package training.coursesloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursesLoaderApplication.class, args);
+    }
+
+}
---
+++ courses-loader/src/test/java/training/coursesloader/CoursesLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package training.coursesloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesLoaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
