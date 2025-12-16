22ce9702c7328959e4522c92f4c640490cdc1dfe
Istvan Viczian <viczian.istvan@gmail.com>
2025-12-16 22:13:25 +0100

    ChatMemoryRepository

--- course/src/main/java/course/AiConfiguration.java
+++ course/src/main/java/course/AiConfiguration.java
@@ -1,15 +1,26 @@
 package course;
 
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration(proxyBeanMethods = false)
 public class AiConfiguration {
 
     @Bean
-    public ChatMemory chatMemory() {
-        return MessageWindowChatMemory.builder().build();
+    public ChatMemory chatMemory(ChatMemoryRepository chatMemoryRepository) {
+        return MessageWindowChatMemory.builder().chatMemoryRepository(chatMemoryRepository).build();
     }
+
+    @Bean
+    public ChatMemoryRepository chatMemoryRepository(JdbcTemplate jdbcTemplate) {
+        return JdbcChatMemoryRepository.builder()
+                .jdbcTemplate(jdbcTemplate)
+                .build();
+    }
+
 }
